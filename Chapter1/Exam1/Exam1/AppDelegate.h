c6a7985e536472bcb981ff67e07675cd98d758f3
Delpini <fdelpini@gmail.com>
2013-11-03 15:36:01 +0900

    Initial Commit

---
+++ Chapter1/Exam1/Exam1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Exam1
+//
+//  Created by 박경준 on 2013. 11. 3..
+//  Copyright (c) 2013년 Delpini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
