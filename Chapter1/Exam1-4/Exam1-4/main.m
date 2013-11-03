80346940dfde65400bd9b4e157fa5b4766b6dde6
Delpini <fdelpini@gmail.com>
2013-11-03 19:36:58 +0900

    exam1-4

---
+++ Chapter1/Exam1-4/Exam1-4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exam1-4
+//
+//  Created by 박경준 on 2013. 11. 3..
+//  Copyright (c) 2013년 Delpini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
