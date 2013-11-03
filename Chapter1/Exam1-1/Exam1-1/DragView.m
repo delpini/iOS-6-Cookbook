aee9dadc46f518dd48167089c098c6dd9e96ca16
Delpini <fdelpini@gmail.com>
2013-11-03 16:56:01 +0900

    commit

---
+++ Chapter1/Exam1-1/Exam1-1/DragView.m
@@ -0,0 +1,31 @@
+//
+//  DragView.m
+//  Exam1-1
+//
+//  Created by 박경준 on 2013. 11. 3..
+//  Copyright (c) 2013년 Delpini. All rights reserved.
+//
+
+#import "DragView.h"
+
+@implementation DragView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Chapter1/Exam1-1/Exam1-1/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  Exam1-1
+//
+//  Created by 박경준 on 2013. 11. 3..
+//  Copyright (c) 2013년 Delpini. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
